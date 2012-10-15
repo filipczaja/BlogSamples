7cce1288b10f738a342804cccf39dfcb91dace33
Filip <f.czaja@kainos.com>
2012-10-15 14:47:52 +0200

    Mocking WCF client with Moq - sample code

---
+++ MockingWCFClientWithMoq/SampleServiceClient.Tests/StringHelperTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SampleServiceClient.SampleWebService;
+
+namespace SampleServiceClient.Tests
+{
+    [TestClass]
+    public class StringHelperTests
+    {
+        [TestMethod]
+        public void TestStringHelper_Reverse()
+        {
+            // Create channel mock
+            Mock<IStringServiceChannel> channelMock = new Mock<IStringServiceChannel>(MockBehavior.Strict);
+
+            // setup the mock to expect the Reverse method
+            channelMock.Setup(c => c.ReverseString("abc")).Returns("cba");
+
+            // create string helper and invoke the Reverse method
+            StringHelper sh = new StringHelper(channelMock.Object);
+            string result = sh.Reverse("abc");
+            Assert.AreEqual("cba", result);
+
+            //verify that the method was called on the mock
+            channelMock.Verify(c => c.ReverseString("abc"), Times.Once());
+        }
+
+        [TestMethod]
+        public void TestStringHelper_Integration()
+        {
+            StringHelper sh = new StringHelper();
+            string result = sh.Reverse("abc");
+            Assert.AreEqual("cba", result);
+        }
+    }
+}
